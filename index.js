13fad7874e3e1eb1f71eb6e8102865ecdcddccf3
Phrank Lenoci <fcl1118@live.com>
2021-12-26 14:35:57 -0600

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require("express"),
   app = express(),
   port = process.env.PORT || 3000,
   bodyParser = require("body-parser");
-
+var cors = require("cors");
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
